7570bf4f0b371ef259b13ad3f14d960fea868402
Yulia Klimova <klimava27@gmail.com>
2021-07-02 17:36:31 +0300

    Editing new posts by user (haven't fixed yet)

--- taskmanager/main/forms.py
+++ taskmanager/main/forms.py
@@ -1,7 +1,17 @@
 from .models import Post
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, Textarea
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
-        fields = ["title", "form"]
+        fields = ['title', 'post']
+        widgets = {
+            'title': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter title'
+            }),
+            'post': Textarea(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter post'
+            })
+        }
