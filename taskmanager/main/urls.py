2f7b1c0cc1207b04f4d4a0d8225c8492afb2d61e
Yulia Klimova <klimava27@gmail.com>
2021-07-09 11:06:42 +0300

    Added dynamical webpages

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about'),
     path('add', views.add, name='add'),
+    path('<int:pk>', views.PostDetailView.as_view(), name='post-detail'),
+    path('<int:pk>', views.PostUpdateView.as_view(), name='update'),
 ]
